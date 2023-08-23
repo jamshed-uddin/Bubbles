2ed004514291240d2897d1ffdf09012d67d30941
Jami <jamsheduddin03@gmail.com>
2023-08-23 19:38:33 +0600

    mongoose connected to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const app = express();
 dotenv.config();
+connectDB();
 const port = process.env.PORT || 3000;
 
 // middleware
